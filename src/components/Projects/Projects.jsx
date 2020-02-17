424ad848ca46ac50285f7359e23b29a744d52ba0
mccoyweb <erik.mccoy@gmail.com>
2020-02-16 22:36:06 -0500

    update take 3

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -38,7 +38,7 @@ const Projects = () => {
 						>
 						<div className="project-wrapper__image">
 							<a
-							href={url || '#!'}
+							href='#!'
 							target="_blank"
 							aria-label="Project Link"
 							rel="noopener noreferrer"
@@ -57,36 +57,23 @@ const Projects = () => {
 						}}
 						>
 						<div data-tilt className="thumbnail rounded">
-							<ProjectImg alt={title} filename={img} />
+							<ProjectImg alt="" filename={img} />
 						</div>
 						<div className="project-wrapper__text">
-							<h3 className="project-wrapper__text-title">{title || 'Project Title'}</h3>
+							<h3 className="project-wrapper__text-title">{'Project Title'}</h3>
 							<div>
 								<p>
-									{info ||
-										'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+									'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'
 									</p>
-									<p className="mb-4">{info2 || ''}</p>
 								</div>
 								<a
 								target="_blank"
 								rel="noopener noreferrer"
 								className="cta-btn cta-btn--hero"
-								href={url || '#!'}
+								href='#!'
 								>
 								See Live
 							</a>
-
-							{repo && (
-								<a
-								target="_blank"
-								rel="noopener noreferrer"
-								className="cta-btn text-color-main"
-								href={repo}
-								>
-								Source Code
-							</a>
-							)}
 						</div>
 					</Tilt>
 				</a>
