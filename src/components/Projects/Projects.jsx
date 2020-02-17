f23713a37c946d099150bd8605fcf125013537b4
mccoyweb <erik.mccoy@gmail.com>
2020-02-16 22:58:28 -0500

    test

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -24,65 +24,7 @@ const Projects = () => {
 
 	return (
   <section id="projects">
-    <Container>
-      <div className="project-wrapper">
-        <Title title="Projects" />
-        <Row>
-          <Col lg={6} sm={12}>
-            <Fade
-            right={isDesktop}
-            bottom={isMobile}
-            duration={1000}
-            delay={1000}
-            distance="30px"
-            >
-              <div className="project-wrapper__image">
-                <a
-                href='#'
-                target="_blank"
-                aria-label="Project Link"
-                rel="noopener noreferrer"
-                >
-                  <Tilt
-                  options={{
-                  reverse: false,
-                  max: 8,
-                  perspective: 1000,
-                  scale: 1,
-                  speed: 300,
-                  transition: true,
-                  axis: null,
-                  reset: true,
-                  easing: 'cubic-bezier(.03,.98,.52,.99)',
-                  }}
-                  >
-                    <div data-tilt className="thumbnail rounded">
-                      <ProjectImg alt="" filename={ProjectImg} />
-                    </div>
-                    <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{'Project Title'}</h3>
-                      <div>
-                        <p>
-                        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'
-                        </p>
-                      </div>
-                      <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn cta-btn--hero"
-                      href='#'
-                      >
-                      See Live
-                      </a>
-                    </div>
-                  </Tilt>
-                </a>
-              </div>
-            </Fade>
-          </Col>
-        </Row>
-      </div>
-    </Container>
+  
   </section>
 );
 
