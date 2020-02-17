35a55d4ed526bdbfc496e9f80644b0ba67c3d8db
mccoyweb <erik.mccoy@gmail.com>
2020-02-16 22:39:29 -0500

    update take 4

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -57,7 +57,7 @@ const Projects = () => {
 						}}
 						>
 						<div data-tilt className="thumbnail rounded">
-							<ProjectImg alt="" filename={img} />
+							<ProjectImg alt="" filename="" />
 						</div>
 						<div className="project-wrapper__text">
 							<h3 className="project-wrapper__text-title">{'Project Title'}</h3>
